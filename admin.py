4cf393511ad5d6bdce47a419324d6ed4127ee535
jshiriyev <jshiriyev@utexas.edu>
2021-06-28 18:32:58 +0400

    first version of admin is ready to use

--- admin.py
+++ admin.py
@@ -32,14 +32,14 @@ class adminGUI():
         sheet_teachers = wb["instructors"]
         sheet_lectures = wb["lectures"]
 
-        teachers.tuple = tuple(sheet_teachers.iter_cols(min_row=1,max_row=11,
+        teachers.tuple = tuple(sheet_teachers.iter_cols(min_row=1,max_row=18,
                                      min_col=1,max_col=4,values_only=True))
 
-        courses.tuple = tuple(sheet_lectures.iter_cols(min_row=2,max_row=5,
+        courses.tuple = tuple(sheet_lectures.iter_cols(min_row=2,max_row=47,
                                      min_col=2,max_col=6,values_only=True))
         
-        hours.tuple = tuple(sheet_lectures.iter_rows(min_row=2,max_row=5,
-                                     min_col=7,max_col=17,values_only=True))
+        hours.tuple = tuple(sheet_lectures.iter_rows(min_row=2,max_row=47,
+                                     min_col=7,max_col=22,values_only=True))
 
         self.teachers = teachers
         self.courses = courses
@@ -67,6 +67,14 @@ class adminGUI():
             
             setattr(self.courses,header,body)
 
+        name = np.char.add(self.courses.code,"-")
+        
+        name = np.char.add(name,self.courses.semester_number.astype(str))
+
+        name = np.char.add(name,"-")
+        
+        self.courses.description = np.char.add(name,self.courses.name_ENG) 
+
     def set_hours(self):
 
         self.hours.definition = self.hours.tuple[0]
@@ -93,36 +101,29 @@ class adminGUI():
         menubar.add_cascade(label="File", menu=fileMenu)
 
         self.set_notebook()
-##        self.set_frame_teachers()
-##        self.set_frame_fall()
-##        self.set_frame_spring()
+        
         self.set_frame_courses()
 
         self.notebook.pack(side=LEFT)
-##        self.frame_teachers.pack(side=LEFT)
-##        self.frame_fall.pack(side=LEFT)
-##        self.frame_spring.pack(side=LEFT)
         self.frame_courses.pack(side=LEFT)
 
     def set_notebook(self):
 
         self.style = ttk.Style(self.root)
 
-        mygreen = "#d2ffd2"
+        configN = {"tabmargins":[2,5,2,0],"tabposition":"wn","background":"white"}
 
-        self.style.theme_create( "yummy", parent="alt",settings={
-            "TNotebook": {"configure": {"tabmargins": [2, 5, 2, 0] } },
-            "TNotebook.Tab": {
-                "configure": {"padding": [5, 1], "background": "white"},
-                "map":       {"background": [("selected", "grey")],
-                              "expand": [("selected", [1, 1, 1, 0])] } } } )
+        configT = {"padding":[5,1],"background":"white"}
+
+        mapT = {"background":[("selected","grey")],
+                    "expand":[("selected",[1,1,1,0])]} 
+        
+        settings = {"TNotebook":{"configure":configN},
+                "TNotebook.Tab":{"configure":configT,"map":mapT}}
+        
+        self.style.theme_create("yummy",parent="alt",settings=settings)
 
         self.style.theme_use("yummy")
-
-        self.style.configure('lefttab.TNotebook',
-                             tabposition='wn',
-                             background="white",
-                             foreground="grey")
 
         self.notebook = ttk.Notebook(self.root,style='lefttab.TNotebook')
 
@@ -130,7 +131,7 @@ class adminGUI():
 
             framename = "frame"+str(i)
 
-            frame = self.set_frame_semester()
+            frame = self.set_frame_notebook()
 
             setattr(self,framename,frame)
             
@@ -138,167 +139,107 @@ class adminGUI():
 
             self.notebook.add(getattr(self,framename),text=name)
 
-    def set_frame_teachers(self):
-
-        self.frame_teachers = Frame(self.root,padx=5,pady=5)
-        self.frame_teachers.configure(background="white")
-        
-        self.frame_teachers.label = Label(self.frame_teachers,text="Instructors")
-        self.frame_teachers.label.configure(background="white")
-        self.frame_teachers.label.pack()
-
-        self.frame_teachers.listbox = Listbox(self.frame_teachers,width=30,height=20)
-        
-        for entry in self.teachers.fullname:
-            self.frame_teachers.listbox.insert(END,entry)
-
-        self.frame_teachers.listbox.pack()
-
-        self.frame_teachers.button = Button(self.frame_teachers,text="Test")
-        self.frame_teachers.button.configure(background="white")
-        self.frame_teachers.button.pack()
-
-    def set_frame_semester(self):
+    def set_frame_notebook(self):
 
         frame = Frame(self.notebook,width=300,height=200)
         frame.configure(background="white")
 
-        frame.label = Label(frame,text="Fall Semester")
-        frame.label.configure(background="white")
-        frame.label.grid(row=0,column=0,columnspan=3)
+        frame.label0 = Label(frame,text="Fall Semester")
+        frame.label0.configure(background="white")
+        frame.label0.grid(row=0,column=0,columnspan=2)
 
-        frame.listbox = Listbox(frame,width=40,height=20)
-        frame.listbox.grid(row=1,column=0,columnspan=3)
+        frame.label1 = Label(frame,text="Spring Semester")
+        frame.label1.configure(background="white")
+        frame.label1.grid(row=0,column=2,columnspan=2)
 
-        frame.button_drop = \
-            Button(frame,text="Drop Selected",
-                   command=lambda: self.move_course(frame.listbox,
-                                                    self.frame_courses.listbox))
+        frame.listbox0 = Listbox(frame,width=40,height=20)
+        frame.listbox0.grid(row=1,column=0,columnspan=2)
         
-        frame.button_drop.configure(background="white")
-        frame.button_drop.grid(row=2,column=0)
+        frame.listbox1 = Listbox(frame,width=40,height=20)
+        frame.listbox1.grid(row=1,column=2,columnspan=2)
+
+        frame.button_drop0 = Button(frame,text="Drop Selected",
+            command=lambda: self.drop_course(frame.listbox0,self.frame_courses.listbox))
         
-        frame.button_clear = \
-            Button(frame,text="Drop All",
-                   command=lambda: self.move_course(frame.listbox,
-                                                    self.frame_courses.listbox,
+        frame.button_drop0.configure(background="white")
+        frame.button_drop0.grid(row=2,column=0,pady=10)
+        
+        frame.button_clear0 = Button(frame,text="Drop All",
+            command=lambda: self.drop_course(frame.listbox0,self.frame_courses.listbox,
                                                     moveall=True))
         
-        frame.button_clear.configure(background="white")
-        frame.button_clear.grid(row=2,column=1)
+        frame.button_clear0.configure(background="white")
+        frame.button_clear0.grid(row=2,column=1,pady=10)
 
-        frame.button_save = \
-            Button(frame,text="Save Courses",command=self.save)
+        frame.button_drop1 = Button(frame,text="Drop Selected",
+            command=lambda: self.drop_course(frame.listbox1,self.frame_courses.listbox))
         
-        frame.button_save.configure(background="white")
-        frame.button_save.grid(row=2,column=2)
+        frame.button_drop1.configure(background="white")
+        frame.button_drop1.grid(row=2,column=2,pady=10)
+        
+        frame.button_clear1 = Button(frame,text="Drop All",
+            command=lambda: self.drop_course(frame.listbox1,self.frame_courses.listbox,
+                                                    moveall=True))
+        
+        frame.button_clear1.configure(background="white")
+        frame.button_clear1.grid(row=2,column=3,pady=10)
+
+        frame.button_test = Button(frame,text="Test Instructor's Schedule",
+                                   command=self.test_load)
+        
+        frame.button_test.configure(background="white")
+        frame.button_test.grid(row=3,column=0,columnspan=4,pady=10)
 
         return frame
 
-    def set_frame_fall(self):
-        
-        self.frame_fall = ttk.Frame(self.root,padx=5,pady=5,width=10,height=20)
-        self.frame_fall.configure(background="white")
-        
-        self.frame_fall.label = Label(self.frame_fall,text="Fall Semester")
-        self.frame_fall.label.configure(background="white")
-        self.frame_fall.label.grid(row=0,column=0,columnspan=3)
-
-        self.frame_fall.listbox = Listbox(self.frame_fall,width=40,height=20)
-        self.frame_fall.listbox.grid(row=1,column=0,columnspan=3)
-
-        self.frame_fall.button_drop = \
-            Button(self.frame_fall,text="Drop Selected",
-                   command=lambda: self.move_course(self.frame_fall.listbox,
-                                                    self.frame_courses.listbox))
-        
-        self.frame_fall.button_drop.configure(background="white")
-        self.frame_fall.button_drop.grid(row=2,column=0)
-        
-        self.frame_fall.button_clear = \
-            Button(self.frame_fall,text="Drop All",
-                   command=lambda: self.move_course(self.frame_fall.listbox,
-                                                    self.frame_courses.listbox,
-                                                    moveall=True))
-        
-        self.frame_fall.button_clear.configure(background="white")
-        self.frame_fall.button_clear.grid(row=2,column=1)
-
-        self.frame_fall.button_save = \
-            Button(self.frame_fall,text="Save Courses",command=self.save)
-        
-        self.frame_fall.button_save.configure(background="white")
-        self.frame_fall.button_save.grid(row=2,column=2)
-
-    def set_frame_spring(self,main):
-        
-        self.frame_spring = Frame(main,padx=5,pady=5,width=10,height=20)
-        self.frame_spring.configure(background="white")
-        
-        self.frame_spring.label = Label(self.frame_spring,text="Spring Semester")
-        self.frame_spring.label.configure(background="white")
-        self.frame_spring.label.grid(row=0,column=0,columnspan=3)
-        
-        self.frame_spring.listbox = Listbox(self.frame_spring,width=40,height=20)
-        self.frame_spring.listbox.grid(row=1,column=0,columnspan=3)
-
-        self.frame_spring.button_drop = \
-            Button(self.frame_spring,text="Drop Selected",
-                   command=lambda: self.move_course(self.frame_spring.listbox,
-                                                    self.frame_courses.listbox))
-        
-        self.frame_spring.button_drop.configure(background="white")
-        self.frame_spring.button_drop.grid(row=2,column=0)
-
-        self.frame_spring.button_clear = \
-            Button(self.frame_spring,text="Drop All",
-                   command=lambda: self.move_course(self.frame_spring.listbox,
-                                                    self.frame_courses.listbox,
-                                                    moveall=True))
-
-        self.frame_spring.button_clear.configure(background="white")
-        self.frame_spring.button_clear.grid(row=2,column=1)
-
-        self.frame_spring.button_save = Button(self.frame_spring,
-                                               text="Save Courses",
-                                               command=self.save)
-        
-        self.frame_spring.button_save.configure(background="white")
-        self.frame_spring.button_save.grid(row=2,column=2)
-
     def set_frame_courses(self):
         
-        self.frame_courses = Frame(self.root,padx=5,pady=5,width=10,height=20)
+        self.frame_courses = Frame(self.root,padx=5,width=10,height=20)
         self.frame_courses.configure(background="white")
 
         self.frame_courses.label = Label(self.frame_courses,text="Courses")
         self.frame_courses.label.configure(background="white")
-        self.frame_courses.label.grid(row=0,column=0,columnspan=2)
+        self.frame_courses.label.grid(row=0,column=0,columnspan=3)
 
         self.frame_courses.listbox = Listbox(self.frame_courses,width=40,height=20)
         
-        for entry in self.courses.code:
+        for entry in self.courses.description:
             self.frame_courses.listbox.insert(END,entry)
             
-        self.frame_courses.listbox.grid(row=1,column=0,columnspan=2)
+        self.frame_courses.listbox.grid(row=1,column=0,columnspan=3)
+
+        idx = self.notebook.index(self.notebook.select())
 
         self.frame_courses.button_tofall = \
             Button(self.frame_courses,text="Add to Fall",
-                   command=lambda: self.move_course(self.frame_courses.listbox,
-                                                    self.frame_fall.listbox))
+                   command=lambda: self.add_course("Fall"))
 
         self.frame_courses.button_tofall.configure(background="white")
-        self.frame_courses.button_tofall.grid(row=2,column=0)
+        self.frame_courses.button_tofall.grid(row=2,column=0,pady=10)
 
         self.frame_courses.button_tospring = \
             Button(self.frame_courses,text="Add to Spring",
-                   command=lambda: self.move_course(self.frame_courses.listbox,
-                                                    self.frame_spring.listbox))
+                   command=lambda: self.add_course("Spring"))
 
         self.frame_courses.button_tospring.configure(background="white")
-        self.frame_courses.button_tospring.grid(row=2,column=1)
+        self.frame_courses.button_tospring.grid(row=2,column=1,pady=10)
 
-    def move_course(self,frombox,tobox,moveall=False):
+        self.frame_courses.button_extended = Button(self.frame_courses,
+                                                    text="Extended View")
+
+        self.frame_courses.button_extended.configure(background="white")
+        self.frame_courses.button_extended.grid(row=2,column=2,pady=10)
+
+        self.frame_courses.status_text = StringVar()
+
+        self.frame_courses.status = Label(self.frame_courses,
+                                          relief="sunken",
+                                          textvariable=self.frame_courses.status_text)
+        
+        self.frame_courses.status.configure(background="white")
+        self.frame_courses.status.grid(row=3,column=0,columnspan=3,pady=10,sticky=EW)
+
+    def drop_course(self,frombox,tobox,moveall=False):
 
         if moveall:
             for entry in frombox.get(0,END):
@@ -308,6 +249,43 @@ class adminGUI():
         elif frombox.curselection():
             tobox.insert(END,frombox.get(frombox.curselection()))
             frombox.delete(frombox.curselection())
+
+    def add_course(self,semester):
+
+        idx = self.notebook.index(self.notebook.select())
+
+        frameID = "frame"+str(idx)
+
+        frombox = self.frame_courses.listbox
+
+        if semester == "Fall":
+            tobox = getattr(self,frameID).listbox0
+        elif semester == "Spring":
+            tobox = getattr(self,frameID).listbox1 
+
+        tobox.insert(END,frombox.get(frombox.curselection()))
+        frombox.delete(frombox.curselection())
+
+    def test_load(self):
+
+        idx = self.notebook.index(self.notebook.select())
+
+        frameID = "frame"+str(idx)
+
+        s0 = getattr(self,frameID).listbox0.get(0,END)
+        s1 = getattr(self,frameID).listbox1.get(0,END)
+
+        annual_load = np.array([np.array(s0+s1)]).T
+
+        idy = np.argwhere(np.array([self.courses.description])==annual_load)[:,1]
+            
+        hours = schedule.hours.nparray[idy,:]
+
+        hours_sum = hours.sum(axis=0)
+
+        status = "total hours is "+str(int(hours_sum[8]))
+
+        self.frame_courses.status_text.set(status)
 
     def save(self):
         
