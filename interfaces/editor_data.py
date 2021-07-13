995ac22d7f5faa6ea2423a84b737b9c15ee75d10
Javid Shiriyev <shiriyevcavid@gmail.com>
2021-07-13 17:09:32 +0400

    working on data file management

--- interfaces/editor_data.py
+++ interfaces/editor_data.py
@@ -110,103 +110,99 @@ class datafile_manager():
 
     """
 
-    def __init__(self,filename,sheets,*args):
-        
-        _,file_extension = os.path.splitext(filename)
+    def __init__(self,filepath,*args):
 
-        if file_extension == ".csv":
-            readcsv(filename,sheets,*args)
-        elif file_extension == ".xlsx":
-            readxlsx(filename,sheets,*args)
+        self.path = filepath
 
-    def reading(self):
+        self.name = self.path.split("\\")[-1]
         
-        with open(inputfile0) as readfile:
+        self.extension = os.path.splitext(self.path)[1]
+
+        if self.extension == ".csv":
+            self.read_csv(*args)
+        elif self.extension == ".xlsx":
+            self.read_xlsx(*args)
+        else:
+            self.read_notype()
+
+##    def read_notype(self,skiplines=0):
+##        
+##        with open(self.path) as notyperead:
+##            self.content = notyperead.readlines()
+##            for linenumber in range(len(self.content)):
+##                if linenumber>=skiplines-1
+##            self.header = self.content[skiplines].split("\n")[0].split("\t")
+##            for linenumber in range(
+##
+##    def todatetime(self,dateformat='%m/%d/%Y',skiplines=1):
+##
+##        self.date = []
+##
+##        for linenumber in range(len(self.content)):
+##            if linenumber>=skiplines:
+##                string = self.content[linenumber].split("\t")[1]
+##                self.date.append(datetime.datetime.strptime(string,dateformat))
+##
+##    def tonumpy(self):
+##
+##        self.content = np.array(self.content)
+
+    def read_csv(self,*args):
+
+        with open(self.path,"r") as csvread:
+            pass
+##            reader = list(csv.reader(csvfile))
+##            reader = np.array(reader)
+##            num_row, num_col = reader.shape
+##            if sheets['dataTypes']=='col':
+##                tuples = tuple(reader.T)
+##            elif sheets['dataTypes']=='row':
+##                tuples = tuple(reader)
+##            setparent(tuples,*args)
             
-            rdate = np.array([])
-            rcopy = np.array([])
-
-            rlines = readfile.readlines()
-
-            for rlinenumber,rline in enumerate(rlines):
-
-                if rlinenumber>0:
-
-                    alist = rline.split('\t')
-
-                    wellname = alist[0]
-
-                    date_list = alist[1].split('-')
-
-                    if date_list[1]=='01':
-                        date_newformat = date_list[2]+' JAN '+date_list[0]
-                    elif date_list[1]=='02':
-                        date_newformat = date_list[2]+' FEB '+date_list[0]
-                    elif date_list[1]=='03':
-                        date_newformat = date_list[2]+' MAR '+date_list[0]
-                    elif date_list[1]=='04':
-                        date_newformat = date_list[2]+' APR '+date_list[0]
-                    elif date_list[1]=='05':
-                        date_newformat = date_list[2]+' MAY '+date_list[0]
-                    elif date_list[1]=='06':
-                        date_newformat = date_list[2]+' JUN '+date_list[0]
-                    elif date_list[1]=='07':
-                        date_newformat = date_list[2]+' JUL '+date_list[0]
-                    elif date_list[1]=='08':
-                        date_newformat = date_list[2]+' AUG '+date_list[0]
-                    elif date_list[1]=='09':
-                        date_newformat = date_list[2]+' SEP '+date_list[0]
-                    elif date_list[1]=='10':
-                        date_newformat = date_list[2]+' OCT '+date_list[0]
-                    elif date_list[1]=='11':
-                        date_newformat = date_list[2]+' NOV '+date_list[0]
-                    elif date_list[1]=='12':
-                        date_newformat = date_list[2]+' DEC '+date_list[0]
-
-                    rdate = np.append(rdate,date_newformat)
-                        
-                    days = alist[2]
-                    condensate = alist[3]
-                    gas = alist[4]
-                    water = alist[5]
-
-                    if rlinenumber+1==len(rlines):
-                        copied_line = '\t\''+wellname+'\'\tSTOP\tGRAT\t'+condensate+'\t'+water+'\t'+gas+' /\n'
-                    else:
-                        copied_line = '\t\''+wellname+'\'\tOPEN\tGRAT\t'+condensate+'\t'+water+'\t'+gas+' /\n'
-
-                    rcopy = np.append(rcopy,copied_line)
-
-    def readcsv(filename,sheets,*args):
-
-        with open(filename) as csvfile:
-            reader = list(csv.reader(csvfile))
-            reader = np.array(reader)
-            num_row, num_col = reader.shape
-            if sheets['dataTypes']=='col':
-                tuples = tuple(reader.T)
-            elif sheets['dataTypes']=='row':
-                tuples = tuple(reader)
-            setparent(tuples,*args)
-            
-    def readxlsx(filename,sheets,*args):
+    def read_xlsx(self,sheets,*args):
         
-        all_tuples = ()
-        wb = openpyxl.load_workbook(filename)
-        for i,sheetname in enumerate(sheets["names"]):
-            sheet = wb[sheetname]
-            if sheets['dataTypes'][i]=='col':
-                tuples = tuple(sheet.iter_cols(max_col=sheets['num_cols'][i],values_only=True))
-            elif sheets['dataTypes'][i]=='row':
-                tuples = tuple(sheet.iter_rows(max_col=sheets['num_cols'][i],values_only=True))
-            all_tuples = all_tuples+tuples
-        setparent(all_tuples,*args)
+        wb = openpyxl.load_workbook(self.path)
 
-    def skiptoline(file_read,keyword,file_written=None):
+##        all_tuples = ()
+##        
+##        for i,sheetname in enumerate(sheets["names"]):
+##            sheet = wb[sheetname]
+##            if sheets['dataTypes'][i]=='col':
+##                tuples = tuple(sheet.iter_cols(max_col=sheets['num_cols'][i],values_only=True))
+##            elif sheets['dataTypes'][i]=='row':
+##                tuples = tuple(sheet.iter_rows(max_col=sheets['num_cols'][i],values_only=True))
+##            all_tuples = all_tuples+tuples
+##        setparent(all_tuples,*args)
+        
+    def write_notype(self):
+            
+        with open(self.path,"w") as notypewritten:
+            pass
+##            with open(outputfile,"w") as writtenfile:
+##            for i,date in enumerate(rdate):
+##                skiptoline(rewrittenfile,'DATES',writtenfile)
+##                skiptoline(rewrittenfile,rdate[i],writtenfile)
+##                skiptoline(rewrittenfile,'WCONHIST',writtenfile)
+##                while True:
+##                    wline = next(rewrittenfile)
+##                    if wline.split('/')[0].strip().split(' ')[0] == '\'BHR_76\'':
+##                        notypewritten.write(rcopy[i])
+##                        break
+##                    else:
+##                        notypewritten.write(wline)
+##            while True:
+##                try:
+##                    copiedline = next(rewrittenfile)
+##                    writtenfile.write(copiedline)
+##                except:
+##                    break
+
+    def skiptoline(self,file_read,keyword,file_written=None):
     
         while True:
             
-            line = next(file_read)
+            line = next(self)
 
             if file_written is not None:
                 file_written.write(line)
@@ -215,37 +211,18 @@ class datafile_manager():
                 break
 
     def setparent(tuples,*args):
+        
         for i in range(len(tuples)):
             for arg in args:
                 if tuples[i][0] == arg.__name__:
                     setchild(tuples[i],arg)
 
-    def setchild(atuple,obj):
+    def setchild(self,atuple):
+        
         array = np.array(atuple[3:]).astype('float64')
-        setattr(obj,atuple[1]+'_unit',atuple[2])
-        setattr(obj,atuple[1],array)
+        setattr(self,atuple[1]+'_unit',atuple[2])
+        setattr(self,atuple[1],array)
 
-    def writing(self):
-            
-        with open(inputfile1) as rewrittenfile:
-            with open(outputfile,"w") as writtenfile:
-                for i,date in enumerate(rdate):
-                    skiptoline(rewrittenfile,'DATES',writtenfile)
-                    skiptoline(rewrittenfile,rdate[i],writtenfile)
-                    skiptoline(rewrittenfile,'WCONHIST',writtenfile)
-                    while True:
-                        wline = next(rewrittenfile)
-                        if wline.split('/')[0].strip().split(' ')[0] == '\'BHR_76\'':
-                            writtenfile.write(rcopy[i])
-                            break
-                        else:
-                            writtenfile.write(wline)
-                while True:
-                    try:
-                        copiedline = next(rewrittenfile)
-                        writtenfile.write(copiedline)
-                    except:
-                        break
 
 class Window(QtWidgets.QMainWindow):
 
