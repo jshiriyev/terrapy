4fad0f7c3bff5429787943c79102cf5a221719fb
Javid Shiriyev <shiriyevcavid@gmail.com>
2021-12-09 20:10:27 +0400

    speeded up little bit, but still does not work

--- flow/pormed/balhoff.py
+++ flow/pormed/balhoff.py
@@ -18,9 +18,12 @@ from interfaces.items import Fluids
 
 class IMPES():
 	
-	def __init__(self):
+	def __init__(self,res,fluids,wells,relperm):
 
-		pass
+		self.res 	= res 
+		self.fluids = fluids
+		self.wells 	= wells
+		self.rp 	= relperm
 
 	def merge(self):
 
@@ -79,35 +82,59 @@ class IMPES():
 
 		self.time_array = np.arange(self.time_step,self.time_total+self.time_step,self.time_step)
 
-	def solve(self):
+	def set_transmissibility(self):
 
-		# print(self.res.grid_indices)
+		dx0 = self.res.grid_sizes[:,0]
+		dy0 = self.res.grid_sizes[:,1]
+		dz0 = self.res.grid_sizes[:,2]
 
-		dx = self.res.grid_sizes[:,0]
-		dy = self.res.grid_sizes[:,1]
-		dz = self.res.grid_sizes[:,2]
+		dxm = self.res.grid_sizes[self.res.grid_indices[:,1],0]
+		dxp = self.res.grid_sizes[self.res.grid_indices[:,2],0]
+		dym = self.res.grid_sizes[self.res.grid_indices[:,3],1]
+		dyp = self.res.grid_sizes[self.res.grid_indices[:,4],1]
+		dzm = self.res.grid_sizes[self.res.grid_indices[:,5],2]
+		dzp = self.res.grid_sizes[self.res.grid_indices[:,6],2]
 
 		Ax = self.res.grid_areas[:,0]
 		Ay = self.res.grid_areas[:,1]
 		Az = self.res.grid_areas[:,2]
 
-		kx = self.res.permeability[:,0]
-		ky = self.res.permeability[:,1]
-		kz = self.res.permeability[:,2]
+		kx0 = self.res.permeability[:,0]
+		ky0 = self.res.permeability[:,1]
+		kz0 = self.res.permeability[:,2]
 
-		dx_m = (dx+self.res.grid_sizes[self.res.grid_indices[:,1],0])/2
-		dx_p = (dx+self.res.grid_sizes[self.res.grid_indices[:,2],0])/2
-		dy_m = (dy+self.res.grid_sizes[self.res.grid_indices[:,3],1])/2
-		dy_p = (dy+self.res.grid_sizes[self.res.grid_indices[:,4],1])/2
-		dz_m = (dz+self.res.grid_sizes[self.res.grid_indices[:,5],2])/2
-		dz_p = (dz+self.res.grid_sizes[self.res.grid_indices[:,6],2])/2
+		kxm = self.res.permeability[self.res.grid_indices[:,1],0]
+		kxp = self.res.permeability[self.res.grid_indices[:,2],0]
+		kym = self.res.permeability[self.res.grid_indices[:,3],1]
+		kyp = self.res.permeability[self.res.grid_indices[:,4],1]
+		kzm = self.res.permeability[self.res.grid_indices[:,5],2]
+		kzp = self.res.permeability[self.res.grid_indices[:,6],2]
 
-		kx_m = (2*dx_m)/(dx/kx+self.res.grid_sizes[self.res.grid_indices[:,1],0]/self.res.permeability[self.res.grid_indices[:,1],0])
-		kx_p = (2*dx_p)/(dx/kx+self.res.grid_sizes[self.res.grid_indices[:,2],0]/self.res.permeability[self.res.grid_indices[:,2],0])
-		ky_m = (2*dy_m)/(dy/ky+self.res.grid_sizes[self.res.grid_indices[:,3],1]/self.res.permeability[self.res.grid_indices[:,3],1])
-		ky_p = (2*dy_p)/(dy/ky+self.res.grid_sizes[self.res.grid_indices[:,4],1]/self.res.permeability[self.res.grid_indices[:,4],1])
-		kz_m = (2*dz_m)/(dz/kz+self.res.grid_sizes[self.res.grid_indices[:,5],2]/self.res.permeability[self.res.grid_indices[:,5],2])
-		kz_p = (2*dz_p)/(dz/kz+self.res.grid_sizes[self.res.grid_indices[:,6],2]/self.res.permeability[self.res.grid_indices[:,6],2])
+		dxm_mean = (dx0+dxm)/2
+		dxp_mean = (dx0+dxp)/2
+		dym_mean = (dy0+dym)/2
+		dyp_mean = (dy0+dyp)/2
+		dzm_mean = (dz0+dzm)/2
+		dzp_mean = (dz0+dzp)/2
+
+		kxm_mean = (2*dxm_mean)/(dx0/kx0+dxm/kxm)
+		kxp_mean = (2*dxp_mean)/(dx0/kx0+dxp/kxp)
+		kym_mean = (2*dym_mean)/(dy0/ky0+dym/kym)
+		kyp_mean = (2*dyp_mean)/(dy0/ky0+dyp/kyp)
+		kzm_mean = (2*dzm_mean)/(dz0/kz0+dzm/kzm)
+		kzp_mean = (2*dzp_mean)/(dz0/kz0+dzp/kzp)
+
+		self.TXM = (Ax*kxm_mean)/(dxm_mean)
+		self.TYM = (Ay*kym_mean)/(dym_mean)
+		self.TZM = (Az*kzm_mean)/(dzm_mean)
+
+		self.TXP = (Ax*kxp_mean)/(dxp_mean)
+		self.TYP = (Ay*kyp_mean)/(dyp_mean)
+		self.TZP = (Az*kzp_mean)/(dzp_mean)
+
+	def solve(self):
+
+		# print(self.res.grid_indices)
 
 		muw = self.fluids.viscosity[0]
 		muo = self.fluids.viscosity[1]
@@ -134,56 +161,62 @@ class IMPES():
 
 		skin = self.wells.skinfactors[windex]
 
-		for time in self.time_array[:2]:
+		Vp = self.res.grid_volumes*self.res.porosity
+
+		cxm_0 = self.res.grid_indices[self.res.grid_hasxmin,0]
+		cxm_m = self.res.grid_indices[self.res.grid_hasxmin,1]
+
+		cxp_0 = self.res.grid_indices[self.res.grid_hasxmax,0]
+		cxp_p = self.res.grid_indices[self.res.grid_hasxmax,2]
+
+		cym_0 = self.res.grid_indices[self.res.grid_hasymin,0]
+		cym_m = self.res.grid_indices[self.res.grid_hasymin,3]
+
+		cyp_0 = self.res.grid_indices[self.res.grid_hasymax,0]
+		cyp_p = self.res.grid_indices[self.res.grid_hasymax,4]
+
+		czm_0 = self.res.grid_indices[self.res.grid_haszmin,0]
+		czm_m = self.res.grid_indices[self.res.grid_haszmin,5]
+
+		czp_0 = self.res.grid_indices[self.res.grid_haszmax,0]
+		czp_p = self.res.grid_indices[self.res.grid_haszmax,6]
+
+		mshape = (self.res.grid_numtot,self.res.grid_numtot)
+		vshape = (self.res.grid_numtot,1)
+
+		vzeros = np.zeros(len(self.wells.itemnames),dtype=int)
+
+		cqw = np.logical_and(~bhp,water)
+		cqo = np.logical_and(~bhp,oil)
+
+		for time in self.time_array:
 
 			print("@{:5.1f}th time step".format(time))
 
-			d11 = (self.res.grid_volumes*self.res.porosity*self.Sw)/(fvfw*self.time_step)*(cr+cw)
-			d12 = (self.res.grid_volumes*self.res.porosity)/(self.time_step*fvfw)
-			d21 = (self.res.grid_volumes*self.res.porosity*(1-self.Sw))/(fvfo*self.time_step)*(cr+co)
-			d22 = (self.res.grid_volumes*self.res.porosity)/(self.time_step*fvfo)*-1
+			d11 = (Vp*self.Sw)/(self.time_step*fvfw)*(cr+cw)
+			d12 = (Vp)/(self.time_step*fvfw)
+			d21 = (Vp*(1-self.Sw))/(self.time_step*fvfo)*(cr+co)
+			d22 = (Vp)/(self.time_step*fvfo)*-1
 
 			self.D = diags(-d22/d12*d11+d21)
 
 			self.rp.system2phase(Sw=self.Sw,model="oil-water")
 
-			TXMw = (Ax*kx_m*self.rp.krw)/(dx_m*muw*fvfw)*6.33e-3 # unit conversion
-			TYMw = (Ay*ky_m*self.rp.krw)/(dy_m*muw*fvfw)*6.33e-3 # unit conversion
-			TZMw = (Az*kz_m*self.rp.krw)/(dz_m*muw*fvfw)*6.33e-3 # unit conversion
+			TXMw = (self.TXM*self.rp.krw)/(muw*fvfw)*6.33e-3 # unit conversion
+			TYMw = (self.TYM*self.rp.krw)/(muw*fvfw)*6.33e-3 # unit conversion
+			TZMw = (self.TZM*self.rp.krw)/(muw*fvfw)*6.33e-3 # unit conversion
 
-			TXPw = (Ax*kx_p*self.rp.krw)/(dx_p*muw*fvfw)*6.33e-3 # unit conversion
-			TYPw = (Ay*ky_p*self.rp.krw)/(dy_p*muw*fvfw)*6.33e-3 # unit conversion
-			TZPw = (Az*kz_p*self.rp.krw)/(dz_p*muw*fvfw)*6.33e-3 # unit conversion
+			TXPw = (self.TXP*self.rp.krw)/(muw*fvfw)*6.33e-3 # unit conversion
+			TYPw = (self.TYP*self.rp.krw)/(muw*fvfw)*6.33e-3 # unit conversion
+			TZPw = (self.TZP*self.rp.krw)/(muw*fvfw)*6.33e-3 # unit conversion
 
-			TXMn = (Ax*kx_m*self.rp.kro)/(dx_m*muo*fvfo)*6.33e-3 # unit conversion
-			TYMn = (Ay*ky_m*self.rp.kro)/(dy_m*muo*fvfo)*6.33e-3 # unit conversion
-			TZMn = (Az*kz_m*self.rp.kro)/(dz_m*muo*fvfo)*6.33e-3 # unit conversion
+			TXMn = (self.TXM*self.rp.kro)/(muo*fvfo)*6.33e-3 # unit conversion
+			TYMn = (self.TYM*self.rp.kro)/(muo*fvfo)*6.33e-3 # unit conversion
+			TZMn = (self.TZM*self.rp.kro)/(muo*fvfo)*6.33e-3 # unit conversion
 
-			TXPn = (Ax*kx_p*self.rp.kro)/(dx_p*muo*fvfo)*6.33e-3 # unit conversion
-			TYPn = (Ay*ky_p*self.rp.kro)/(dy_p*muo*fvfo)*6.33e-3 # unit conversion
-			TZPn = (Az*kz_p*self.rp.kro)/(dz_p*muo*fvfo)*6.33e-3 # unit conversion
-
-			cxm_0 = self.res.grid_indices[self.res.grid_hasxmin,0]
-			cxm_m = self.res.grid_indices[self.res.grid_hasxmin,1]
-
-			cxp_0 = self.res.grid_indices[self.res.grid_hasxmax,0]
-			cxp_p = self.res.grid_indices[self.res.grid_hasxmax,2]
-
-			cym_0 = self.res.grid_indices[self.res.grid_hasymin,0]
-			cym_m = self.res.grid_indices[self.res.grid_hasymin,3]
-
-			cyp_0 = self.res.grid_indices[self.res.grid_hasymax,0]
-			cyp_p = self.res.grid_indices[self.res.grid_hasymax,4]
-
-			czm_0 = self.res.grid_indices[self.res.grid_haszmin,0]
-			czm_m = self.res.grid_indices[self.res.grid_haszmin,5]
-
-			czp_0 = self.res.grid_indices[self.res.grid_haszmax,0]
-			czp_p = self.res.grid_indices[self.res.grid_haszmax,6]
-
-			print(cxm_m)
-
-			mshape = (self.res.grid_numtot,self.res.grid_numtot)
+			TXPn = (self.TXP*self.rp.kro)/(muo*fvfo)*6.33e-3 # unit conversion
+			TYPn = (self.TYP*self.rp.kro)/(muo*fvfo)*6.33e-3 # unit conversion
+			TZPn = (self.TZP*self.rp.kro)/(muo*fvfo)*6.33e-3 # unit conversion
 
 			self.Tw = csr(mshape)
 
@@ -227,26 +260,19 @@ class IMPES():
 
 			self.T = diags(-d22/d12)*self.Tw+self.Tn
 
-			Jw_v = (2*np.pi*dz[gindex]*kx[gindex]*self.rp.krw[gindex])/(muw*fvfw*(np.log(self.req/rw)+skin))*6.33e-3 # unit conversion
-			Jn_v = (2*np.pi*dz[gindex]*kx[gindex]*self.rp.kro[gindex])/(muo*fvfo*(np.log(self.req/rw)+skin))*6.33e-3 # unit conversion
+			Jw_v = (2*np.pi*self.res.grid_sizes[:,2][gindex]*self.res.permeability[:,0][gindex]*self.rp.krw[gindex])/(muw*fvfw*(np.log(self.req/rw)+skin))*6.33e-3 # unit conversion
+			Jn_v = (2*np.pi*self.res.grid_sizes[:,2][gindex]*self.res.permeability[:,0][gindex]*self.rp.kro[gindex])/(muo*fvfo*(np.log(self.req/rw)+skin))*6.33e-3 # unit conversion
 
 			self.Jw = csr((Jw_v[bhp],(gindex[bhp],gindex[bhp])),shape=mshape)
 			self.Jn = csr((Jn_v[bhp],(gindex[bhp],gindex[bhp])),shape=mshape)
 
 			self.J = diags(-d22/d12)*self.Jw+self.Jn
-
-			vshape = (self.res.grid_numtot,1)
-
-			vzeros = np.zeros(len(self.wells.itemnames),dtype=int)
 			
 			self.Qw = csr(vshape)
 			self.Qn = csr(vshape)
 
 			self.Qw += csr((QB[bhp]*Jw_v[bhp],(gindex[bhp],vzeros[bhp])),shape=vshape)
 			self.Qn += csr((QB[bhp]*Jn_v[bhp],(gindex[bhp],vzeros[bhp])),shape=vshape)
-
-			cqw = np.logical_and(~bhp,water)
-			cqo = np.logical_and(~bhp,oil)
 
 			self.Qw += csr((QB[cqw]*5.61,(gindex[cqw],vzeros[cqw])),shape=vshape) # unit conversion
 			self.Qn += csr((QB[cqo]*5.61,(gindex[cqo],vzeros[cqo])),shape=vshape) # unit conversion
@@ -281,80 +307,77 @@ class SS():
 
 if __name__ == "__main__":
 
-	solver = IMPES()
-
 	# FORMATION
 
-	solver.res = Formation("FU",None)
+	res = Formation("FU",None)
 
-	solver.res.set_dimensions((1200,600,200))
+	res.set_dimensions((1200,600,200))
 
-	solver.res.grid((3,3,1))
+	res.grid((3,3,1))
 
-	solver.res.set_porosity(0.26)
+	res.set_porosity(0.26)
 
-	solver.res.set_permeability(1800)
+	res.set_permeability(1800)
 
-	solver.res.set_compressibility(3e-6)
+	res.set_compressibility(3e-6)
 
 	# FLUID
 
-	solver.fluids = Fluids(2)
+	fluids = Fluids(2)
 
-	solver.fluids.set_names(("water","oil"))
-	solver.fluids.set_compressibility((2e-6,5e-6))
-	solver.fluids.set_viscosity((1.,5.))
-	solver.fluids.set_fvf((1.,1.))
+	fluids.set_names(("water","oil"))
+	fluids.set_compressibility((2e-6,5e-6))
+	fluids.set_viscosity((1.,5.))
+	fluids.set_fvf((1.,1.))
 
 	# WELLS
 
-	solver.wells = Wells(None)
+	wells = Wells(None)
 
-	solver.wells.set_names(["inj","prod1","prod2"])
+	wells.set_names(["inj","prod1","prod2"])
 
-	solver.wells.set_tracks((
+	wells.set_tracks((
 		(( 600,300,400),( 600,300,0)),
 		(( 200,100,400),( 200,100,0)),
 		((1000,500,400),(1000,500,0)),
 		))
 
-	solver.wells.set_flowconds(
+	wells.set_flowconds(
 		conditions=("rate","rate","bhp"),
 		limits=(3000,-2000,800),
 		fluids=("water","oil","both"))
 
-	solver.wells.set_skinfactors((0,0,0))
+	wells.set_skinfactors((0,0,0))
 
-	solver.wells.set_radii((0.5,0.5,0.5))
+	wells.set_radii((0.5,0.5,0.5))
 
 	# RELATIVE PERMEABILITY
 
-	# solver.rp = relative_permeability(
- #                 Sorow=0.2,
- #                 Swc=0.2,
- #                 krowc=1,
- #                 krwor=0.2,
- #                 no=3,
- #                 nw=3)
+	# relperm = relative_permeability(
+	# 	Sorow=0.2,
+	# 	Swc=0.2,
+	# 	krowc=1,
+	# 	krwor=0.2,
+	# 	no=3,
+	# 	nw=3)
 
-	solver.rp = relative_permeability_balhoff(
-			Swi=0.2,
-			Swr=0.2,
-			krwo=0.2,
-			kroo=1.0,
-			nw=3,
-			no=3,
-			)
-
-	# Sw = np.linspace(0.2,0.8)
-
-	# solver.rp.system2phase(Sw=Sw,model="oil-water")
+	relperm = relative_permeability_balhoff(
+		Swi=0.2,
+		Swr=0.2,
+		krwo=0.2,
+		kroo=1.0,
+		nw=3,
+		no=3,
+		)
 
 	# SOLVING
+
+	solver = IMPES(res,fluids,wells,relperm)
 
 	solver.merge()
 	solver.initialize(pressure=1000,saturation=0.2)
 	solver.set_time(0.1,500)
+	solver.set_transmissibility()
 	solver.solve()
 
 	# PLOTTING
