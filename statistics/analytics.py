ccf37fffd6f35b9d429de56b79ae3fdf7315fa4e
Javid Shiriyev <jshiriyev@utexas.edu>
2022-02-13 23:09:42 +0400

    update analytics

--- statistics/analytics.py
+++ statistics/analytics.py
@@ -108,42 +108,56 @@ class heterogeneity():
         
         return coefficient
 
-def correlation(self):
+class correlation():
 
-    X = self.x
-    Y = self.y
+    def coefficient(self):
 
-    N = X.shape[0]
+        X = self.x
+        Y = self.y
 
-    std_X = np.sqrt(1/(N-1)*np.sum((X-X.mean())**2))
-    std_Y = np.sqrt(1/(N-1)*np.sum((Y-Y.mean())**2))
-        
-    cov_XY = 1/(N-1)*np.sum((X-X.mean())*(Y-Y.mean()))  
-    rho_XY = cov_XY/(std_X*std_Y)
-        
-    return rho_XY
+        N = X.shape[0]
 
-def qqplot(self):
+        std_X = np.sqrt(1/(N-1)*np.sum((X-X.mean())**2))
+        std_Y = np.sqrt(1/(N-1)*np.sum((Y-Y.mean())**2))
+            
+        cov_XY = 1/(N-1)*np.sum((X-X.mean())*(Y-Y.mean()))  
+        rho_XY = cov_XY/(std_X*std_Y)
+            
+        return rho_XY
 
-    ##A = np.random.normal(25,10,100)
-    ##B = np.random.normal(25,5,100)
+    def qqplot(self):
 
-    percentile = np.linspace(0,100,101)
+        ##A = np.random.normal(25,10,100)
+        ##B = np.random.normal(25,5,100)
 
-    fA = np.percentile(A,percentile)
-    fB = np.percentile(B,percentile)
+        percentile = np.linspace(0,100,101)
 
-    zmin = np.min((fA.min(),fB.min()))
-    zmax = np.max((fA.max(),fB.max()))
+        fA = np.percentile(A,percentile)
+        fB = np.percentile(B,percentile)
 
-     plt.plot(np.array([zmin,zmax]),np.array([zmin,zmax]),'--',c='k')
-     plt.scatter(fA,fB,c='r')
+        zmin = np.min((fA.min(),fB.min()))
+        zmax = np.max((fA.max(),fB.max()))
 
-     plt.xlabel('A',fontsize=14)
-     plt.ylabel('B',fontsize=14)
+         plt.plot(np.array([zmin,zmax]),np.array([zmin,zmax]),'--',c='k')
+         plt.scatter(fA,fB,c='r')
 
-     ax = plt.gca()
-     ax.set_aspect('equal')
+         plt.xlabel('A',fontsize=14)
+         plt.ylabel('B',fontsize=14)
+
+         ax = plt.gca()
+         ax.set_aspect('equal')
+
+class clustering():
+
+    pass
+
+class association():
+
+    pass
+
+class dimensional_reduction():
+
+    pass
 
 if __name__ == "__main__":
 
